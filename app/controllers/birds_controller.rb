cd1ef97d3ac515e62c2c5a4fbbe4c9aadd77cbee
Stephen Talley <stvlley.amzn@gmail.com>
2022-04-06 09:57:47 -0400

    commit tree 061c4e7d75f6d7a86ff93f1f040c9b170d109114

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
 
+  wrap_parameters format: []
+
   # GET /birds
   def index
     birds = Bird.all
@@ -14,6 +16,16 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  def create
+    # byebug
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
